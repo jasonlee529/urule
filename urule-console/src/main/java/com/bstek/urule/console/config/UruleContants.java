083101274c5d24f466256230c3ef167dec7a53c6
luoning@infisa.cn <luoning@infisa.cn>
2020-09-01 16:49:39 +0800

    规则引擎开发V1.0.1 1. 发送知识库请求配置修改

--- urule-console/src/main/java/com/bstek/urule/console/config/UruleContants.java
+++ urule-console/src/main/java/com/bstek/urule/console/config/UruleContants.java
@@ -11,7 +11,7 @@ public interface UruleContants {
 
     Map<String, String> KM_SERVER = new HashMap<String, String>() {{
         this.put("dev", "http://192.168.1.22:8081");
-        this.put("base", "http://192.168.1.215:32150");
-        this.put("proView", "http://192.168.1.218:32150");
+        this.put("base", "http://192.168.1.215:33110");
+        this.put("proView", "http://192.168.1.218:33110");
     }};
 }
